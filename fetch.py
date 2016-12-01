c58a557fa1aa5d24f2f2a9006cab523895ea9d61
Mukul Surajiwale <mukul@carn-walk-wl-77.dynamic2.rpi.edu>
2016-12-01 18:24:11 -0500

    Initial commit

---
+++ fetch.py
@@ -0,0 +1,96 @@
+from urllib2 import urlopen
+from flask import Flask, jsonify
+from pytube import YouTube
+from xml.etree import ElementTree
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+from sets import Set
+
+import os
+import sys
+import numpy
+import math
+import json
+import subprocess
+import re
+
+entities = {}
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    print "Main page hit"
+    return jsonify(status="ok")
+
+# Get video captions
+@app.route("/captions/<id>")
+def processCaptions(id=None):
+    video_url = "https://www.youtube.com/watch?v=" + id
+    # navigate to path
+    path = "/Users/mukul/Dropbox/RPI/Fall 2016/Natural Language Processing/Term Project/Project/captions"
+    os.chdir(path)
+    # get captions in srt
+    f = os.popen("youtube-dl --list-subs --skip-download --output " + "'" + id + ".%(ext)s' " + video_url)
+    info = f.read()
+    print info
+    searchObj = re.search(r'(no subtitles)', info, re.M|re.I)
+    if searchObj:
+        return jsonify(downloaded = False)
+    else:
+        os.system("youtube-dl --write-srt --sub-lang en --skip-download --output " + "'" + id + ".%(ext)s' " + video_url)
+        return jsonify(downloaded = True)
+
+# Extract named entities
+@app.route("/ner/<id>")
+def extractNamedEntities(id=None):
+    st = StanfordNERTagger('/Users/mukul/NLP/stanford-ner-2015-12-09/classifiers/english.muc.7class.distsim.crf.ser.gz',
+					       '/Users/mukul/NLP/stanford-ner-2015-12-09/stanford-ner.jar',
+                           encoding='utf-8')
+
+    # Decode each line to utf-8 into one string.
+    lines_str = ""
+    for line in open("captions/" + id + ".en.vtt", 'r'):
+        if line != '\n' and line[0] != '0':
+            lines_str += line.decode('utf-8').rstrip() + " "
+
+    # Create an array of sentences
+    sents = lines_str.split(".")
+
+    text = ""
+    i = 0
+    texts = []
+    while i < len(sents) - 5:
+        s = " "
+        seq = (sents[i].rstrip() + '.', sents[i + 1].rstrip() + '.', sents[i + 2].rstrip() + '.', sents[i + 3].rstrip() + '.', sents[i + 4].rstrip() + '.')
+        text = s.join(seq)
+        texts.append(text)
+        i += 5
+
+    for text in texts:
+
+        tokenized_text = word_tokenize(text)
+        classified_text = st.tag(tokenized_text)
+
+        # print classified_text
+
+        for word_class in classified_text:
+            word = word_class[0]
+            category = word_class[1]
+
+            if category != 'O':
+                if category not in entities:
+                    entities[category] = [word]
+                else:
+                    if word not in entities[category]:
+                        entities[category].append(word)
+
+
+
+
+    return jsonify(entities)
+
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=int("8000"))
