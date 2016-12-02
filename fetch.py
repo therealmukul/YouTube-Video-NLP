214b27a97b9be3292320107bc96064710191ff25
Mukul Surajiwale <mukul@union-wl-463.dynamic2.rpi.edu>
2016-12-01 20:10:06 -0500

    word grouping working

--- fetch.py
+++ fetch.py
@@ -48,18 +48,21 @@ def extractNamedEntities(id=None):
 					       '/Users/mukul/NLP/stanford-ner-2015-12-09/stanford-ner.jar',
                            encoding='utf-8')
 
+    completed = 0
     # Decode each line to utf-8 into one string.
     lines_str = ""
     for line in open("captions/" + id + ".en.vtt", 'r'):
         if line != '\n' and line[0] != '0':
             lines_str += line.decode('utf-8').rstrip() + " "
 
-    # Create an array of sentences
+    # Create an array of sentences.
     sents = lines_str.split(".")
 
+    # Groups 5 sentences together and extract the named entities.
     text = ""
     i = 0
     texts = []
+    # while i < 10:
     while i < len(sents) - 5:
         s = " "
         seq = (sents[i].rstrip() + '.', sents[i + 1].rstrip() + '.', sents[i + 2].rstrip() + '.', sents[i + 3].rstrip() + '.', sents[i + 4].rstrip() + '.')
@@ -71,26 +74,30 @@ def extractNamedEntities(id=None):
 
         tokenized_text = word_tokenize(text)
         classified_text = st.tag(tokenized_text)
-
-        # print classified_text
-
-        for word_class in classified_text:
-            word = word_class[0]
-            category = word_class[1]
-
+        completed += 5
+        print float(completed)/float(len(sents)) * 100
+        j = 0
+        while j < len(classified_text):
+            category = classified_text[j][1]
             if category != 'O':
+                word = ""
+                while classified_text[j][1] != 'O' and classified_text[j][1] == category:
+                    word += classified_text[j][0] + " "
+                    j += 1
+
+                word = word.strip()
+
                 if category not in entities:
                     entities[category] = [word]
                 else:
                     if word not in entities[category]:
                         entities[category].append(word)
-
-
-
+            else:
+                j += 1
 
     return jsonify(entities)
 
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=int("8000"))
+    app.run(host="localhost", port=int("8000"), threaded=True)
